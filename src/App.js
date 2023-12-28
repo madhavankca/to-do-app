b38273dba7cbe735f768e9810a664e0e84d89f67
madhavan <madhavankca@gmail.com>
2023-12-28 18:55:20 +0530

    todo app after checkpoint 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import "./styles.css";
+import Task from "./Task/Task";
+import Header from "./Header/Header";
+import { useState } from "react";
 
 export default function App() {
+  const [taskList, setTaskList] = useState([
+    "Read SpringBoot",
+    "Complete assignments",
+    "Prepare breakfast",
+    "Sleep for 2 hours",
+    "Take a shower",
+  ]);
+
   return (
-    <div className="Application">
-      <h1>Aye Captain! 🫡</h1>
-      <h2>Lets create something!</h2>
+    <div className="App">
+      <Header />
+
+      {taskList.map((task, index) => (
+        <Task task={task} key={index} />
+      ))}
+      <br />
+
+      <button
+        onClick={() => {
+          setTaskList([...taskList, "New Task"]);
+        }}
+      >
+        Regular Button
+      </button>
+      <br />
+      <br />
+
+      <button
+        onClick={() => {
+          console.log(taskList);
+          setTaskList([...taskList, "Additional Task"]);
+          console.log(taskList);
+        }}
+      >
+        Add Tasks
+      </button>
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          setTaskList([]);
+          console.log("Nothing to do buddy. Sleep!");
+        }}
+      >
+        Empty Tasks
+      </button>
     </div>
   );
 }
---
+++ src/Task/Task.js
@@ -0,0 +1,7 @@
+export default function Task({ task }) {
+  return (
+    <div>
+      <h4>To do: {task}</h4>
+    </div>
+  );
+}
